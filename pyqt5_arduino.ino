109eeda672afda008d0112759400ee5574c496aa
ilker07 <55693489+ilker07@users.noreply.github.com>
2019-09-25 17:21:55 +0300

    Add files via upload

---
+++ pyqt5_arduino.ino
@@ -0,0 +1,37 @@
+
+char veri;
+#define led 4
+#define role 7
+void setup() {
+
+ pinMode(led,OUTPUT);
+  pinMode(role,OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+ int okunan=analogRead(A0);
+ Serial.println(okunan);
+ delay(10);
+
+ if(Serial.available())
+ {
+  veri=Serial.read();
+  if(veri=='1')
+    digitalWrite(led,HIGH);
+    
+  if(veri=='0')
+     digitalWrite(led,LOW);
+     
+   if(veri=='2')
+     digitalWrite(role,HIGH);
+    if(veri=='3')
+     digitalWrite(role,LOW);     
+   veri="";
+ }
+
+  
+
+}
